04e15bbacbefef3992290af3c5b9fbc1f0f2e75e
Sujata-Gupta <sujatagupta312@gmail.com>
2025-03-18 23:37:27 +0530

    Initial commit

---
+++ src/ex_01_Java__Basic/Task1103_printf_Table.java
@@ -0,0 +1,29 @@
+package ex_01_Java__Basic;
+
+public class Task1103_printf_Table {
+    public static void main(String[] args) {
+        int n=9;
+        int count=1;
+
+        System.out.printf("%d X %d = %d%n",n,count,n*count);
+        count++;
+        System.out.printf("%d X %d = %d%n",n,count,n*count);
+        count++;
+        System.out.printf("%d X %d = %d%n",n,count,n*count);
+        count++;
+        System.out.printf("%d X %d = %d%n",n,count,n*count);
+        count++;
+        System.out.printf("%d X %d = %d%n",n,count,n*count);
+        count++;
+        System.out.printf("%d X %d = %d%n",n,count,n*count);
+        count++;
+        System.out.printf("%d X %d = %d%n",n,count,n*count);
+        count++;
+        System.out.printf("%d X %d = %d%n",n,count,n*count);
+        count++;
+        System.out.printf("%d X %d = %d%n",n,count,n*count);
+        count++;
+        System.out.printf("%d X %d = %d%n",n,count,n*count);
+
+    }
+}
