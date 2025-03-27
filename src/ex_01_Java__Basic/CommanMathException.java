60f69779415e979e260ffa8596440f9bf02b9593
Sujata-Gupta <sujatagupta312@gmail.com>
2025-03-27 15:50:41 +0530

    push

---
+++ src/ex_01_Java__Basic/CommanMathException.java
@@ -0,0 +1,9 @@
+package ex_01_Java__Basic;
+
+public class CommanMathException {
+    public static void main(String[] args) {
+
+        System.out.println(10/0); //"main" java.lang.ArithmeticException
+
+    }
+}
---
+++ src/ex_01_Java__Basic/Println_VS_Print.java
@@ -0,0 +1,10 @@
+package ex_01_Java__Basic;
+
+public class Println_VS_Print {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.print("hello");
+        System.out.print("world");
+    }
+}
