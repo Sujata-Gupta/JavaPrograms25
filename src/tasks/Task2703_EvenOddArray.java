0f6a58e3c50e60f5b2232c3eb755e62c90a5e1c7
Sujata-Gupta <sujatagupta312@gmail.com>
2025-04-27 00:00:27 +0530

    push

---
+++ src/tasks/Task2703_EvenOddArray.java
@@ -0,0 +1,16 @@
+package tasks;
+
+public class Task2703_EvenOddArray {
+    public static void main(String[] args) {
+        int[] num={12,234,12,65,2,67,99};
+                for (int i:num)
+                {
+                    if(i%2==0)
+                    {
+                        System.out.println(i+" is even");
+                    }
+                    else
+                        System.out.println(i+" odd");
+                }
+    }
+}
