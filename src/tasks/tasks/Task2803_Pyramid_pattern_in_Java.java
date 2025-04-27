28c69af2426af84cc0afacfd0e050f628c09fa1b
Sujata-Gupta <sujatagupta312@gmail.com>
2025-04-27 19:19:28 +0530

    push

--- src/tasks/tasks/Task2803_Pyramid_pattern_in_Java.java
+++ src/tasks/tasks/Task2803_Pyramid_pattern_in_Java.java
@@ -12,16 +12,16 @@ public class Task2803_Pyramid_pattern_in_Java {
         System.out.println("Enter num");
        int n=sc.nextInt();
 
-        for (int i = 0; i <n ; i++)
+        for (int i = 1; i <n ; i++)
         {
             for (int j=n;j>i;j--) {
                 System.out.print(" ");
 
             }
 
-            for (int k=0;k<i+1;k++)
+            for (int k=0;k<i*2-1;k++)//1,3,5
             {
-                System.out.print("* ");
+                System.out.print("*");
             }
             System.out.println();
 
---
+++ src/tasks/tasks/task_pyramid.java
@@ -0,0 +1,37 @@
+package tasks.tasks;
+
+import java.util.Scanner;
+
+public class task_pyramid {
+
+        public static void main(String[] args) {
+            //    *
+            //    **
+            //    ***
+            //   *****
+            //  *******
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter num");
+            int n=sc.nextInt();
+
+            for (int i = 0; i <n ; i++)
+            {
+                for (int j=n;j>i;j--) {
+                    System.out.print(" ");
+
+                }
+
+                for (int k=0;k<i+1;k++)
+                {
+                    System.out.print("* ");
+                }
+                System.out.println();
+
+
+            }
+
+        }
+    }
+
+
+
